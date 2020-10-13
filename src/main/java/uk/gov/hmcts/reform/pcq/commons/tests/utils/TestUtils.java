c5b75c5f0a6634a8829ad8a39e0263e56a5a796a
Manoj Kulkarni <manoj.kulkarni@cgi.com>
2020-10-13 12:56:42 +0100

    Moved TestUtils to test.utilspackage

--- src/main/java/uk/gov/hmcts/reform/pcq/commons/tests/utils/TestUtils.java
+++ src/main/java/uk/gov/hmcts/reform/pcq/commons/tests/utils/TestUtils.java
@@ -1,4 +1,4 @@
-package uk.gov.hmcts.reform.pcq.commons.utils;
+package uk.gov.hmcts.reform.pcq.commons.tests.utils;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.core.io.ClassPathResource;
