3510baeeca4036bc113e93087f1814565cd93397
Rapolas Kaselis <rapolas.kaselis@hmcts.net>
2023-06-30 14:49:04 +0100

    PCQ-1762 bind ConfigurationProperties on a Bean

--- src/main/java/uk/gov/hmcts/reform/pcq/commons/config/FeignInterceptorConfiguration.java
+++ src/main/java/uk/gov/hmcts/reform/pcq/commons/config/FeignInterceptorConfiguration.java
@@ -3,6 +3,7 @@ package uk.gov.hmcts.reform.pcq.commons.config;
 import feign.RequestInterceptor;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
@@ -15,6 +16,7 @@ import java.util.Locale;
 public class FeignInterceptorConfiguration {
 
     @Bean
+    @ConfigurationProperties(prefix = "feign.allow")
     public RequestInterceptor requestInterceptor(FeignHeaderConfig config) {
         return requestTemplate -> {
             ServletRequestAttributes attrs = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
